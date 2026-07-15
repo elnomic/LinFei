541ec8555e0be70c7ca749965fab1cb05de9f2aa
elnomic <elnomic@yahoo.com>
2026-07-16 06:46:46 +0700

    Refactor Supabase client initialization

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,5 +1,10 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export const createClient = () => {
-  return createClientComponentClient()
+  return supabase
 }
